e780ba2f370ea6ebc1075b6ded28064ad95a7b74
satish-rajnale <satishrajnale98@gmail.com>
2021-05-23 02:07:51 +0530

    main body

---
+++ src/components/MainBody/MainBody.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`
+
+
+export const SideBar = styled.div`
+width: 3em;
+height: 100%;
+background-color:#3b3b44;
+`
---
+++ src/components/MainBody/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container, SideBar} from "./MainBody"
+function MainBody() {
+    return (
+        <Container>
+            <SideBar/>
+        </Container>
+    )
+}
+
+export default MainBody
