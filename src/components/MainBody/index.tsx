d135c0d12489c1def373e4777bebd4b6adb3408d
satish-rajnale <satishrajnale98@gmail.com>
2021-05-23 11:23:24 +0530

    index

--- src/components/MainBody/index.tsx
+++ src/components/MainBody/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import {Container, SideBar} from "./MainBody"
+import { FaUserCircle } from "react-icons/fa";
+
+import {
+    AccountContainer,
+  
+  } from "../../styles/components/Header";
 export default function MainBody() {
     return (
         <Container>
             <SideBar>
-                side
+            <AccountContainer>
+          <FaUserCircle size={34} color="#625f5f" fill="#625f5f" />
+        </AccountContainer>
             </SideBar>
             sfdgdrgdrgv
         </Container>
